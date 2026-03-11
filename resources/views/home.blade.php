ce74ec45361613e02eb7c6c11bc48ec2af669952
zackyy-1 <zackyilhamm12@gmail.com>
2026-03-12 00:04:23 +0700

    feature: navbar landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-
-<body>
-    <nav>
-        <div></div>
-        <div>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </div>
-    </nav>
+@extends('layouts.public')
+@section('content')
     <header>
 
     </header>
+
     <section>
         
     </section>
-</body>
-
-</html>
+@endsection
